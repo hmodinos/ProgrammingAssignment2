4bc6ef90651765b6d9429b828060cdbb2b1c45d2
Henrique Martins <Henrique@07hw004445s-MacBook-Pro.local>
2015-02-19 00:53:15 -0200

    Coursera Exercise
    Complete functions

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,18 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y ## assign the matrix y to the  x in parent environment
+    m <<- NULL ## initialize m in the parent environment
+  }
+  get <- function() x 
+  setinverse <- function(inverse) m <<- inverse ## set the cache m to the inverse of the matrix x
+  getinverse <- function() m 
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
@@ -12,4 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      m <- x$getinverse()
+      if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinverse(m)
+      m
 }
